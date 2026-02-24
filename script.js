2bc3f5bc72d114923983d9128472d8f321e0493c
ALEX HENRIQUE DA CRUZ GOMES <25132769@CFP127ALUNO.LOCAL>
2026-02-24 17:00:17 -0300

    criação dos elementos dos botões dos cursos realizado com sucesso.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict'
-const main = document.getElementById('main')
+const main = document.querySelector('main')
 
 const acharCursos = async function() {
     const url = "https://lion-school-phbo.onrender.com/cursos"
@@ -7,6 +7,14 @@ const acharCursos = async function() {
     const cursos = await response.json()
     return cursos
 
+}
+
+const acharAlunos = async function(cursoID){
+    const url = `https://lion-school-phbo.onrender.com/alunos?curso_id=${cursoID}`
+    const response = await fetch(url)
+    const alunos = await response.json()
+    console.log(alunos)
+    return alunos
 }
 
 const criarBotoes = async function() {
@@ -18,11 +26,25 @@ const criarBotoes = async function() {
     cursos.forEach(item => {
         
         const botao = document.createElement('button')
-
-        botao.classList.add('b1')
+        const img = document.createElement('img')
+        if(item.sigla == 'DS'){
+            img.src = './img/DS.svg'
+            botao.textContent = item.sigla
+        }else{
+            img.src = './img/REDES.svg'
+            botao.textContent = item.sigla
+        }
         
-        botao.textContent = cursos.sigla
-        botoes.appendChild(botao)
+        img.classList.add('imagem-curso')
+        botao.classList.add('b1')
+        botao.append(img)
+        botoes.append(botao)
+
+        botao.addEventListener('click', async () => {
+            main.textContent = ''
+            const cursoID = item.id
+            acharAlunos(cursoID)
+        })
 
         
     });
@@ -32,3 +54,7 @@ const criarBotoes = async function() {
     
 }
 criarBotoes()
+
+const criarTelaAlunos = async function () {
+    
+}
