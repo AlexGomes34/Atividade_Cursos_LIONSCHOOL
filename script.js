940f4664c1d9a21efd572ea7fb93facfe8bf193f
AlexGomes34 <lekinhog9@gmail.com>
2026-02-24 21:52:20 -0300

    foi finalizada a primeira e segunda tela do projeto, agora so faltam a tela de perfil e os testes do jest

--- script.js
+++ script.js
@@ -13,11 +13,42 @@ const acharAlunos = async function(cursoID){
     const url = `https://lion-school-phbo.onrender.com/alunos?curso_id=${cursoID}`
     const response = await fetch(url)
     const alunos = await response.json()
-    console.log(alunos)
     return alunos
 }
 
-const criarBotoes = async function() {
+const acharAlunoSozinho = async function(alunoID){
+    const url = `https://lion-school-phbo.onrender.com/alunos/${alunoID}`
+    const response = await fetch(url)
+    const aluno = await response.json()
+    console.log(aluno)
+    return aluno
+}
+
+const criarTela1 = async function() {
+
+    const computador = document.createElement('div')
+    const cursoTexto = document.createElement('h1');
+    const texto1 = document.createTextNode('Escolha um ');
+    const span = document.createElement('span');
+    span.textContent = 'curso';
+    const br = document.createElement('br');
+    const texto2 = document.createTextNode(' para gerenciar');
+    cursoTexto.appendChild(texto1);
+    cursoTexto.appendChild(span);
+    cursoTexto.appendChild(br);
+    cursoTexto.appendChild(texto2);
+
+    const compImg = document.createElement('img')
+    compImg.src = './img/devices.svg'
+    const estudante = document.createElement('img')
+    estudante.src = './img/studant.svg'
+
+    computador.classList.add('computador')
+    compImg.classList.add('comp')
+    estudante.classList.add('estuda')
+
+    main.append(computador, estudante)
+    computador.append(cursoTexto, compImg)
 
     const botoes = document.createElement('div')
     botoes.classList.add('botoes')
@@ -44,6 +75,7 @@ const criarBotoes = async function() {
             main.textContent = ''
             const cursoID = item.id
             acharAlunos(cursoID)
+            criarTelaAlunos(cursoID)
         })
 
         
@@ -53,8 +85,56 @@ const criarBotoes = async function() {
 
     
 }
-criarBotoes()
+criarTela1()
 
-const criarTelaAlunos = async function () {
+const criarTelaAlunos = async function (cursoID) {
     
+    const container = document.createElement('div')
+    const cursoNome = document.createElement('div')
+    const nomeCurso = document.createElement('h1')
+    const todosAlunos = document.createElement('div')
+
+    container.classList.add('container')
+    cursoNome.classList.add('cursoNome')
+    nomeCurso.classList.add('nomeCurso')
+    todosAlunos.classList.add('todosAlunos')
+
+    main.appendChild(container)
+    container.append(cursoNome, todosAlunos)
+
+    if (cursoID === 1) {
+        nomeCurso.textContent = 'Desenvolvimento de Sistemas'
+    } else if (cursoID === 2) {
+        nomeCurso.textContent = 'Redes'
+    }
+
+    const alunos = await acharAlunos(cursoID)
+    alunos.forEach(item => {
+        const aluno = document.createElement('div')
+        const fotoAluno = document.createElement('img')
+        const nomeAluno = document.createElement('h1')
+        aluno.classList.add('alunos')
+        fotoAluno.classList.add('foto1')
+        nomeAluno.classList.add('nomeAluno')
+
+        nomeAluno.textContent = item.nome
+        fotoAluno.src = item.foto
+
+        todosAlunos.appendChild(aluno)
+        aluno.append(fotoAluno, nomeAluno)
+
+        aluno.addEventListener('click', async () => {
+        main.textContent = ''
+        const alunoID = item.id
+        acharAlunoSozinho(alunoID)
+        criarTelaAlunoSozinho(alunoID)
+    })
+
+    })
+    
+   
+}
+
+const criarTelaAlunoSozinho = async function() {
+
 }
